8fbfd4cab2db15f58c0391417708082e68e568b1
Alex <alexxxgor87@gmail.com>
2025-12-30 15:16:51 +0200

    Introduce syntax error to test CI/CD failure detection

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 console.log("Start the test"); 
-setTimeout(() => console.log("Wating 3 seconds"), 3000); 
+setTimeout(() => console.log("Wating 3 seconds", 3000); 
 console.log("Test completed");
 
